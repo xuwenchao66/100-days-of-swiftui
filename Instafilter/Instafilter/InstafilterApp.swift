ea1788b1600d73b7f3dd15c12a39347da83155ae
xuwenchao <842390367@qq.com>
2024-04-21 11:18:57 +0800

    Instafilter

---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by 徐文超 on 2024/4/14.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
