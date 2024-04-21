cbd0a56a37bdf29494c96783ec0adedde1583069
xuwenchao <842390367@qq.com>
2024-04-21 19:42:31 +0800

    BucketList damn....

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by 徐文超 on 2024/4/21.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
